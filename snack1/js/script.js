01607bebc5f276b83f2dacd349de4f452f3b9090
RiccardoPellegrino <pellegrino.riccardo.90@gmail.com>
2022-10-12 15:15:36 +0200

    modifica snack1

--- snack1/js/script.js
+++ snack1/js/script.js
@@ -2,15 +2,18 @@
 // Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
 
 let messaggio = document.querySelector('.messaggio');
+let messaggio2 = document.querySelector('.messaggio2');
 let btn = document.getElementById("bottone");
 
 btn.addEventListener("click", function () {
     
     let numero = document.getElementById('numero').value;
     if (numero % 2 !==0) {
-        numero++;
         console.log('Il numero insierito è il '+ numero);
-        messaggio.innerHTML = "Il numero da stampare è " + numero;
+        messaggio2.innerHTML = "Il numero inserito è " + numero;
+        numero++;
+        console.log('Lo abbiamo arrotondato per il numero pari piu vicino '+ numero);
+        messaggio.innerHTML = "Lo abbiamo arrotondato per il numero pari piu vicino " + numero;
     } else {
         console.log('Il numero insierito è il '+ numero);
         messaggio.innerHTML = "Il numero da stampare è " + numero;
