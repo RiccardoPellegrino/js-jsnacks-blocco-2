b7b29a4b89ece47a179dc1c03aad8f345d47985f
RiccardoPellegrino <pellegrino.riccardo.90@gmail.com>
2022-10-12 16:16:20 +0200

    cancellati input messaggio 1 e 2 inutili

--- snack2/js/script.js
+++ snack2/js/script.js
@@ -1,8 +1,7 @@
 // ** Snack2 **
 //     Generatore di “nomi cognomi” casuali: il Grande Gatsby ha  una lista di nomi e una lista di cognomi, e da queste vuole generare una falsa lista di invitati con nome e cognome. (10 persone)
 
-let messaggio = document.querySelector('.messaggio');
-let messaggio2 = document.querySelector('.messaggio2');
+
 let btn = document.getElementById("generatore");
 
 
