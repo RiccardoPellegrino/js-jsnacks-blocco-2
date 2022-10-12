3cbf4f364445c8ef63fdf6a6c977c246934739f5
RiccardoPellegrino <pellegrino.riccardo.90@gmail.com>
2022-10-12 16:11:34 +0200

    Snack 2

--- snack2/js/script.js
+++ snack2/js/script.js
@@ -1,2 +1,35 @@
 // ** Snack2 **
-//     Generatore di “nomi cognomi” casuali: il Grande Gatsby ha  una lista di nomi e una lista di cognomi, e da queste vuole generare una falsa lista di invitati con nome e cognome.
+//     Generatore di “nomi cognomi” casuali: il Grande Gatsby ha  una lista di nomi e una lista di cognomi, e da queste vuole generare una falsa lista di invitati con nome e cognome. (10 persone)
+
+let messaggio = document.querySelector('.messaggio');
+let messaggio2 = document.querySelector('.messaggio2');
+let btn = document.getElementById("generatore");
+
+let arrayInvitati = [];
+
+btn.addEventListener("click", function () {
+    document.querySelector('#inventato').innerHTML = '';
+    let arrayNomi = ["Riccardo", "Simone", "Stefano", "Mattia", "Stefania", "Michela", "Giulia","Manuel", "Selenia","Giancarlo"];
+    let arrayCognomi = ["Rossi", "Ferrari", "Russo", "Bianchi", "Romano", "Gallo", "Costa","Bianchi","Pinoli"];
+
+    for (let i = 0; i < 10; i++) {
+        let rand = Math.floor(Math.random() * arrayNomi.length);
+        let nomeCasuale = arrayNomi[rand];
+        console.log(nomeCasuale);
+
+        // messaggio.innerHTML = "Il nome generato è " + nomeCasuale;
+
+        let rand2 = Math.floor(Math.random() * arrayNomi.length);
+        let cognomeCasuale = arrayCognomi[rand2];
+        console.log(cognomeCasuale);
+        // messaggio2.innerHTML = "Il cognome generato è " + cognomeCasuale;
+
+        arrayInvitati.push(nomeCasuale + ' ' + cognomeCasuale);
+
+        console.log(arrayInvitati);
+        let paragrafo = document.createElement('p');
+        document.querySelector('#inventato').append(paragrafo);
+        paragrafo.innerHTML = arrayInvitati[i];
+    }
+    arrayInvitati = [];
+})
