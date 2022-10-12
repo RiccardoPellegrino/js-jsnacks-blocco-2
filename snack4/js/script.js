8a388e891626922bb695a402e84f1e13f3c280b7
RiccardoPellegrino <pellegrino.riccardo.90@gmail.com>
2022-10-12 22:30:45 +0200

    snack 4

--- snack4/js/script.js
+++ snack4/js/script.js
@@ -1,2 +1,26 @@
 // **Snack4 (Bonus)**
 // Crea due array che hanno un numero di elementi diversi.  Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
+
+let arrayDue = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let arrayUno = [1, 2, 3, 4, 5,];
+
+if (arrayUno.length > arrayDue.length) {
+    let differenza = (arrayUno.length - 1) - (arrayDue.length - 1);
+
+    for (let i = 0; i < differenza; i++) {
+        let numeroCasuale = (Math.round(Math.random() * 8) + 1);
+        arrayDue.push(numeroCasuale);
+        console.log("Array due "+arrayDue);
+    }
+} else if (arrayDue.length > arrayUno.length) {
+
+    let differenza = (arrayDue.length - 1) - (arrayUno.length - 1);
+    for (let i = 0; i < differenza; i++) {
+        let numeroCasuale = (Math.round(Math.random() * 8) + 1);
+        arrayUno.push(numeroCasuale);
+        console.log("Array uno "+arrayUno);
+    }
+
+} else {
+    console.log("I due array sono della stessa dimensione")
+}
