a24ea2262ed2cad9642d45c18bc39e4373e33eb0
RiccardoPellegrino <pellegrino.riccardo.90@gmail.com>
2022-10-13 10:16:35 +0200

    correzione -1

--- snack4/js/script.js
+++ snack4/js/script.js
@@ -5,7 +5,7 @@ let arrayDue = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 let arrayUno = [1, 2, 3, 4, 5,];
 
 if (arrayUno.length > arrayDue.length) {
-    let differenza = (arrayUno.length - 1) - (arrayDue.length - 1);
+    let differenza = (arrayUno.length) - (arrayDue.length);
 
     for (let i = 0; i < differenza; i++) {
         let numeroCasuale = (Math.round(Math.random() * 8) + 1);
@@ -14,7 +14,7 @@ if (arrayUno.length > arrayDue.length) {
     }
 } else if (arrayDue.length > arrayUno.length) {
 
-    let differenza = (arrayDue.length - 1) - (arrayUno.length - 1);
+    let differenza = (arrayDue.length) - (arrayUno.length);
     for (let i = 0; i < differenza; i++) {
         let numeroCasuale = (Math.round(Math.random() * 8) + 1);
         arrayUno.push(numeroCasuale);
